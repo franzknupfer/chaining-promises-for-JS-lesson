04362385a17f7d92feb48a2a3e185d9f43f48aff
Franz Knupfer <franzknupfer@hotmail.com>
2017-08-10 10:14:21 -0700

    making an API call with XMLHttpRequest

---
+++ weather-interface.js
@@ -0,0 +1,29 @@
+let apiKey = "[API-KEY-GOES-HERE]";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    // Listening for request response.
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    // Opening and sending the request.
+    request.open("GET", url, true);
+    request.send();
+
+    // Callback for displaying the request response in the browser.
+    getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
