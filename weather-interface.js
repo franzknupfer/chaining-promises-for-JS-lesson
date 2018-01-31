991424cc8fb5a0937eef972ced3576cb87dd1e52
Franz Knupfer <franzknupfer@hotmail.com>
2018-01-31 11:14:36 -0800

    complete example

--- weather-interface.js
+++ weather-interface.js
@@ -1,5 +1,5 @@
-let weatherKey = [WEATHER-KEY-HERE];
-let giphyKey = [GIPHY-KEY-HERE];
+let weatherKey = "64ce943b2cb480de60202f7a5a15b25c";
+let giphyKey = "8ef085759e344f9c9cf34b6ab2af0e86";
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
@@ -9,7 +9,7 @@ $(document).ready(function() {
     function weatherCall() {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${[WEATHER-KEY-HERE]}`;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
 
         request.onload = function() {
           if (this.status === 200) {
@@ -27,7 +27,7 @@ $(document).ready(function() {
     function giphyCall(humidity) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `http://api.giphy.com/v1/gifs/search?q=${humidity}&api_key=${[GIPHY-KEY-HERE]}`
+        let url = `http://api.giphy.com/v1/gifs/search?q=${humidity}&api_key=${giphyKey}&limit=5`
 
         request.onload = function() {
           if (this.status === 200) {
